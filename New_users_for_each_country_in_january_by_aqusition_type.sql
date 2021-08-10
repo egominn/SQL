721add76fd761582d994336b651d2e81a1ef440b
egominn <88170196+egominn@users.noreply.github.com>
2021-08-10 13:16:34 +0900

    Create New_users_for_each_country_in_january_by_aqusition_type.sql

---
+++ New_users_for_each_country_in_january_by_aqusition_type.sql
@@ -0,0 +1,14 @@
+SELECT  first_country,
+        DATE(installed_at) AS installed_date,
+        COUNT(DISTINCT CASE WHEN installed_at = created_at AND is_organic = 1 THEN user_id END) AS new_organic_users,
+        COUNT(DISTINCT CASE WHEN installed_at = created_at AND is_organic = 0 THEN user_id END) AS new_non_organic_users
+FROM (
+    SELECT user_id,
+           created_at,
+           installed_at,
+           is_organic,
+           FIRST_VALUE(country) OVER (PARTITION BY user_id ORDER BY created_at) AS first_country
+   FROM events
+   WHERE DATE(installed_at) BETWEEN '2000-01-01' AND '2000-01-31'
+        ) AS a
+GROUP BY first_country, installed_date
