4e03784aaa85d81c7a9baf55aad90e73e3aacbb9
egominn <88170196+egominn@users.noreply.github.com>
2021-08-11 15:40:35 +0900

    Update Conversion_to_payment_for_each_country_in_january.sql

--- Conversion_to_payment_for_each_country_in_january.sql
+++ Conversion_to_payment_for_each_country_in_january.sql
@@ -1,9 +1,9 @@
 -- Посчитать количество конверсий в платежи для каждой страны на каждый день января.
 
 SELECT 
-  DATE(created_at) AS date,
-  first_country,
-  COUNT(DISTINCT CASE WHEN converted_at = created_at THEN user_id END) AS conversions
+    DATE(created_at) AS date,
+    first_country,
+    COUNT(DISTINCT CASE WHEN converted_at = created_at THEN user_id END) AS conversions
 FROM (
         SELECT
             user_id,
@@ -13,6 +13,6 @@ FROM (
             MIN(CASE WHEN event_name = 'purchase' THEN created_at END) OVER (PARTITION BY user_id) AS converted_at
         FROM events
         WHERE DATE(created_at) BETWEEN '2000-01-01' AND '2000-01-31'
-        ) AS a
+     ) AS a
 GROUP BY 1, 2
 ORDER BY 2, 1
